7fd7c38becd2264aab105faca642cfbdfb64b8bd
Kimi Schulte <kimi.schulte019@stud.fh-dortmund.de>
2024-09-30 15:05:47 +0200

    EProg Aufgabe 1 Update für Print

--- src/main/java/dual/info/eprog/uebungen/UEB01.java
+++ src/main/java/dual/info/eprog/uebungen/UEB01.java
@@ -3,7 +3,12 @@ package dual.info.eprog.uebungen;
 public class UEB01 {
 
     public void beispielMethode() {
-        System.out.println("Hier könnte dein Name und deine Matrikelnummer stehen");
+        System.out.println("Hier könnte dein Name und deine Matrikelnummer stehen :)");
+    }
+
+    public static void main(String[] args) {
+        UEB01 ueb01 = new UEB01();
+        ueb01.beispielMethode();
     }
 
 }
