f2912ad011a5da5e5fad7c9b67ce91d99a660bd7
Kimi Schulte <kimi.schulte019@stud.fh-dortmund.de>
2024-09-30 11:02:13 +0200

    init

---
+++ src/main/java/de/kimischulte/Main.java
@@ -0,0 +1,16 @@
+package de.kimischulte;
+
+import de.kimischulte.aufgabe1.Aufgabe1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Aufgabe1 aufgabe1 = new Aufgabe1();
+        System.out.println(aufgabe1.c(0, 0) == 1);
+        System.out.println(aufgabe1.c(0, 1) == 0);
+        System.out.println(aufgabe1.c(1, 1));
+        System.out.println(aufgabe1.c(4, 4));
+        System.out.println(aufgabe1.c(4, 3));
+        System.out.println(aufgabe1.c(3, 2));
+    }
+}
---
+++ src/main/java/de/kimischulte/aufgabe1/Aufgabe1.java
@@ -0,0 +1,16 @@
+package de.kimischulte.aufgabe1;
+
+public class Aufgabe1 {
+
+    public int c(int n, int k) {
+        if(n < 0)
+            throw new IllegalArgumentException("n < 0");
+        if (n == 0) {
+            return k == 0 ? 1 : 0;
+            // fancy if Schreibweise, auch genannt "inline if"
+        } else {
+            return c(n-1, k-1) + c(n-1, k);
+        }
+    }
+
+}
