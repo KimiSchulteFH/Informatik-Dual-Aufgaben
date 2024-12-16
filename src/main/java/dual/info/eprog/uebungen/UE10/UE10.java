4ab40a9b769f6c5fdae9db228b81216b26fc8324
Jan Niklas Schiffer <jan.schiffer112@stud.fh-dortmund.de>
2024-12-16 13:47:53 +0100

    Fortnite Burger

--- src/main/java/dual/info/eprog/uebungen/UE10/UE10.java
+++ src/main/java/dual/info/eprog/uebungen/UE10/UE10.java
@@ -2,15 +2,18 @@ package dual.info.eprog.uebungen.UE10;
 
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class UE10 {
 
     public static void main(String[] args) {
-
+       // aufgabe3();
+        aufgabe4();
     }
 
     public static void aufgabe4() {
-
+        Game game = new Game();
+        game.start();
     }
 
     public static void aufgabe3() {
@@ -26,6 +29,118 @@ public class UE10 {
             abteilung1.addMitarbeiter(mitarbeiter1);
             abteilung2.addMitarbeiter(mitarbeiter2);
             abteilung3.addMitarbeiter(mitarbeiter3);
+        }
+    }
+
+}
+
+class Game {
+
+    private Wanderer[] wanderer;
+    private Taschenlampe taschenlampe;
+
+    public Game() {
+        wanderer = new Wanderer[]{
+                new Wanderer('A', 5),
+                new Wanderer('B', 10),
+                new Wanderer('C', 20),
+                new Wanderer('D', 25),
+        };
+        taschenlampe = new Taschenlampe(60);
+    }
+
+    public void start() {
+        while (!istGewonnen()) {
+            System.out.println("Taschenlampe: " + taschenlampe.minutesLeft);
+            System.out.println("Die Lampe ist " + taschenlampe.position.toString() + ", diese Wanderer können gehen:");
+            for (Wanderer wanderer1 : wanderer) {
+                if(wanderer1.position == taschenlampe.position)
+                    System.out.print(wanderer1.name + "(" + wanderer1.distanceInMinutes + ")   ");
+            }
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("\nWelche Wanderer sollen gehen (1 oder 2 Zeichen zwischen A und D): ");
+            String input = scanner.nextLine();
+            if(input.length() > 2 || !input.matches("[A-D]*"))
+                continue;
+            int slowest = 0;
+            for (char c : input.toCharArray()) {
+                Wanderer wanderer = getWanderer(c);
+                if(wanderer.distanceInMinutes > slowest)
+                    slowest = wanderer.distanceInMinutes;
+                switchPosition(wanderer);
+            }
+            switchPosition(taschenlampe);
+            taschenlampe.useLamp(slowest);
+        }
+        System.out.println("Gewonnen!");
+    }
+
+    public boolean istGewonnen() {
+        for (Wanderer wanderer1 : wanderer) {
+            if(wanderer1.position == Position.Links)
+                return false;
+        }
+        return true;
+    }
+
+    private Wanderer getWanderer(char c) {
+        for (Wanderer wanderer1 : wanderer) {
+            if(wanderer1.name == c && wanderer1.position == taschenlampe.position) {
+                return wanderer1;
+            }
+        }
+        System.out.println("Systemfehler: Falscher Input!");
+        System.exit(69);
+        return null;
+    }
+
+    private void switchPosition(Wanderer wanderer) {
+        if(wanderer.position == Position.Links) {
+            wanderer.position = Position.Rechts;
+        } else {
+            wanderer.position = Position.Links;
+        }
+    }
+
+    private void switchPosition(Taschenlampe lampe) {
+        if(lampe.position == Position.Links) {
+            lampe.position = Position.Rechts;
+        } else {
+            lampe.position = Position.Links;
+        }
+    }
+}
+
+class Wanderer {
+    final char name;
+    final int distanceInMinutes;
+    Position position;
+
+    public Wanderer(char name, int distanceInMinutes) {
+        this.name = name;
+        this.distanceInMinutes = distanceInMinutes;
+        position = Position.Links;
+    }
+}
+enum Position {
+    Rechts, Links
+}
+
+class Taschenlampe {
+
+    int minutesLeft;
+    Position position;
+
+    public Taschenlampe(int minutesLeft) {
+        this.minutesLeft = minutesLeft;
+        position = Position.Links;
+    }
+
+    public void useLamp(int minutes) {
+        this.minutesLeft -= minutes;
+        if(minutesLeft < 0) {
+            System.out.println("Leider verloren");
+            System.exit(69420);
         }
     }
 
