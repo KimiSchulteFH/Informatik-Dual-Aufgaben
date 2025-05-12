f2de89e6567f76695d0f5358854bf972c0374987
Jess <jess.schaefer@bookwire.de>
2025-05-12 10:44:24 +0200

    mathe

--- src/main/java/dual/info/mfi2/UE04.java
+++ src/main/java/dual/info/mfi2/UE04.java
@@ -1,5 +1,7 @@
 package dual.info.mfi2;
 
+import java.sql.SQLOutput;
+
 public class UE04 {
 
     private static float f_1(float y) {
@@ -24,6 +26,30 @@ public class UE04 {
         int b = 5;
         float y = 3.6f;
         System.out.println(locate_y_value(y, a, b));
+        System.out.println(locate_y_value_iterativ(y,a,b));
     }
 
+    public static float locate_y_value_iterativ(float y, float a, float b) {
+        float konstante = 0.1f;
+        while(Math.abs(a-b) > konstante) {
+            if(y < f_1((a-b)/2)) {
+                a = a;
+                b = f_1((a-b)/2);
+            } else if (y > f_1((a-b)/2)) {
+                a = f_1((a-b)/2);
+                b = b;
+            } else {
+                return f_1((a+b)/2);
+            }
+        }
+        return f_1((a+b)/2);
+    }
+
+    public static String unstetige_funktion_easy() {
+        return "f(x) = for (x <0,5) : 0; else: 1";
+    }
+
+
 }
+
+
