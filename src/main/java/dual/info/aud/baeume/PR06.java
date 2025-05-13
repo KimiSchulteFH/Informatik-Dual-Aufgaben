7c91cbfe100ef496e67182642633256b72b3bb3d
tjoost <tjoost@brockhaus-ag.de>
2025-05-13 10:09:09 +0200

    PR06

---
+++ src/main/java/dual/info/aud/baeume/PR06.java
@@ -0,0 +1,9 @@
+package dual.info.aud.baeume;
+
+public class PR06 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/dual/info/aud/baeume/provided/BaumTest.java
@@ -0,0 +1,9 @@
+package dual.info.aud.baeume.provided;
+
+public class BaumTest
+{
+	public static void main(String[] args)
+	{
+		new BaumGUI().setVisible(true);
+	}
+}
