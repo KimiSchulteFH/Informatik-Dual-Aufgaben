eb7228cb05c5fea9dec63010da419734cc53438e
tjoost <tjoost@brockhaus-ag.de>
2025-06-17 10:57:13 +0200

    Praktikum 10

--- src/main/java/dual/info/aud/sortieren/HeapSort.java
+++ src/main/java/dual/info/aud/sortieren/HeapSort.java
@@ -1,18 +1,45 @@
 package dual.info.aud.sortieren;
 
-public class HeapSort
-{
-	// Versickere das Element mit Index pos in dem Teilfeld von Index links bis einschlie�lich Index rechts
-	public static void versickere(int[] array, final int links, int pos, final int rechts)
-	{
-		// Im Gegensatz zur Vorlesung ben�tigt diese Methode vier Parameter, da der zu sortierende Bereich
-		// nicht bei 0, sondern bei links anf�ngt. Linkes Kind von Knoten i: (i-links)*2+links+1 = 2*i+1-links
+public class HeapSort {
 
-		// TODO: Praktikum 10
+	// Versickere das Element mit Index pos im Bereich von links bis rechts
+	public static void versickere(int[] array, final int links, int pos, final int rechts) {
+		while (true) {
+			int leftChild = 2 * (pos - links) + 1 + links;
+			int rightChild = 2 * (pos - links) + 2 + links;
+			int largest = pos;
+
+			if (leftChild <= rechts && array[leftChild] > array[largest]) {
+				largest = leftChild;
+			}
+
+			if (rightChild <= rechts && array[rightChild] > array[largest]) {
+				largest = rightChild;
+			}
+
+			if (largest == pos) break;
+
+			vertausche(array, pos, largest);
+			pos = largest;
+		}
 	}
 
-	public static void heapsort(int[] array, final int links, final int rechts)
-	{
-		// TODO: Praktikum 10
+	public static void heapsort(int[] array, final int links, final int rechts) {
+		// ERSTE PHASE: Heap aufbauen
+		for (int i = (rechts + links - 1) / 2; i >= links; i--) {
+			versickere(array, links, i, rechts);
+		}
+
+		// ZWEITE PHASE: Sortieren
+		for (int i = rechts; i > links; i--) {
+			vertausche(array, links, i); // Max-Element an das Ende
+			versickere(array, links, links, i - 1); // Heap wiederherstellen
+		}
+	}
+
+	private static void vertausche(int[] array, int index1, int index2) {
+		int temp = array[index1];
+		array[index1] = array[index2];
+		array[index2] = temp;
 	}
 }
--- src/main/java/dual/info/aud/sortieren/MergeSort.java
+++ src/main/java/dual/info/aud/sortieren/MergeSort.java
@@ -1,81 +1,85 @@
 package dual.info.aud.sortieren;
 
-public class MergeSort
-{
-	public static void mergesort(int[] array, final int links, final int rechts)
-	{
-		int[] hilfsarray = new int[array.length];
-		
-		mergesortBU(array, hilfsarray, links, rechts);
-	}
-	
-	// Top-Down-Mergesort entsprechend der Vorlesung
-	public static void mergesortTD(int[] array, int[] hilfsarray, final int links, final int rechts)
-	{
-		if (links < rechts)
-		{
-			// mindestens 2 Elemente
-			int mitte = (links + rechts)/2;
-			// Feld in der Mitte teilen und rekursive Aufrufe f�r Teilfelder
-			mergesortTD(array, hilfsarray, links, mitte);
-			mergesortTD(array, hilfsarray, mitte+1, rechts);
-			// Sortierte Teilfelder mischen
-			merge(array, hilfsarray, links, mitte, rechts);
-		}
-	}
+public class MergeSort {
+    public static void mergesort(int[] array, final int links, final int rechts) {
+        int[] hilfsarray = new int[array.length];
 
-	// Bottom-Up-Mergesort
-	public static void mergesortBU(int[] array, int[] hilfsarray, final int links, final int rechts)
-	{
-		// TODO: Praktikum 10
+        mergesortBU(array, hilfsarray, links, rechts);
+    }
 
-	}
+    // Top-Down-Mergesort entsprechend der Vorlesung
+    public static void mergesortTD(int[] array, int[] hilfsarray, final int links, final int rechts) {
+        if (links < rechts) {
+            // mindestens 2 Elemente
+            int mitte = (links + rechts) / 2;
+            // Feld in der Mitte teilen und rekursive Aufrufe f�r Teilfelder
+            mergesortTD(array, hilfsarray, links, mitte);
+            mergesortTD(array, hilfsarray, mitte + 1, rechts);
+            // Sortierte Teilfelder mischen
+            merge(array, hilfsarray, links, mitte, rechts);
+        }
+    }
 
-	// Mischen der Teilfelder array[links]...array[mitte] und array[mitte+1]...array[rechts]
-	private static void merge(int[] array, int[] hilfsarray, final int links, final int mitte, final int rechts)
-	{
-		int i, j;
+    // Bottom-Up-Mergesort
+    public static void mergesortBU(int[] array, int[] hilfsarray, final int links, final int rechts) {
+        // TODO: Praktikum 10
 
-		i = links; 
-		j = mitte+1;
-		
-		// Mischen in Hilfsarray
-		for (int k=links; k <= rechts; k++)
-		{
-			if (i>mitte)  
-				hilfsarray[k] = array[j++]; // 1. Teilfeld schon zu Ende			
-			else if (j>rechts)
-				hilfsarray[k] = array[i++]; // 2. Zeilfeld schon zu Ende
-			else if(array[i] < array[j])
-				hilfsarray[k] = array[i++]; // Element aus 1. Teilfeld �bernehmen
-			else
-				hilfsarray[k] = array[j++];	// Element aus 2. teilfeld �bernehmen						
-		}
+        int laenge = 1;
+        while (links + laenge <= rechts) {
+            int i = links;
+            while (i + laenge <= rechts) {
+                int r = Math.min(i + 2 * laenge - 1, rechts);
+                merge(array, hilfsarray, i, i + laenge - 1, r);
+                i = i + laenge * 2;
+            }
+            laenge = laenge * 2;
+        }
+    }
 
-		
-		for (int k = links; k <= rechts; k++)
-		{
-			array[k] = hilfsarray[k];
-		}
-	}
 
-	// Alternative Metdode zum Mischen
-	private static void merge2(int[] array, int[] hilfsarray, final int links, final int mitte, final int rechts)
-	{
-		int i, j;
+    // Mischen der Teilfelder array[links]...array[mitte] und array[mitte+1]...array[rechts]
+    private static void merge(int[] array, int[] hilfsarray, final int links, final int mitte, final int rechts) {
+        int i, j;
 
-		// 1. Teilfeld kopieren
-		for (i = mitte+1; i > links; i--)
-			hilfsarray[i-1] = array[i-1];
-		// 2. Teilfeld in umgekehrter Reihenfolge kopieren
-		for (j = mitte; j < rechts; j++)
-			hilfsarray[rechts+mitte-j] = array[j+1];
-		
-		// Zeiger i und j gegeneinander laufen lassen
-		for (int k = links; k <= rechts; k++ )
-			if (hilfsarray[j] < hilfsarray[i])
-				array[k] = hilfsarray[j--];
-			else
-				array[k] = hilfsarray[i++];				
-	}
+        i = links;
+        j = mitte + 1;
+
+        // Mischen in Hilfsarray
+        for (int k = links; k <= rechts; k++) {
+            if (i > mitte)
+                hilfsarray[k] = array[j++]; // 1. Teilfeld schon zu Ende
+            else if (j > rechts)
+                hilfsarray[k] = array[i++]; // 2. Zeilfeld schon zu Ende
+            else if (array[i] < array[j])
+                hilfsarray[k] = array[i++]; // Element aus 1. Teilfeld �bernehmen
+            else
+                hilfsarray[k] = array[j++];    // Element aus 2. teilfeld �bernehmen
+        }
+
+
+        for (int k = links; k <= rechts; k++) {
+            array[k] = hilfsarray[k];
+        }
+    }
+
+    // Alternative Metdode zum Mischen
+    private static void merge2(int[] array, int[] hilfsarray, final int links, final int mitte, final int rechts) {
+        int i, j;
+
+        // 1. Teilfeld kopieren
+        for (i = mitte + 1; i > links; i--)
+            hilfsarray[i - 1] = array[i - 1];
+        // 2. Teilfeld in umgekehrter Reihenfolge kopieren
+        for (j = mitte; j < rechts; j++)
+            hilfsarray[rechts + mitte - j] = array[j + 1];
+
+        // Zeiger i und j gegeneinander laufen lassen
+        for (int k = links; k <= rechts; k++)
+            if (hilfsarray[j] < hilfsarray[i])
+                array[k] = hilfsarray[j--];
+            else
+                array[k] = hilfsarray[i++];
+    }
+
 }
+
